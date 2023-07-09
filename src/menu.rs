cc985a4d31c3222b747d7441ef761be5186fc0c5
Claire Pinlac <pinlacclaire19@gmail.com>
2023-07-09 13:56:22 +1200

    added exit button in the menu

--- src/menu.rs
+++ src/menu.rs
@@ -48,7 +48,36 @@ fn menu_setup(mut commands: Commands, asset_server: Res<AssetServer>) {
                 })
                 .with_children(|parent| {
                     parent.spawn(TextBundle::from_section(
-                        "Button",
+                        "Play",
+                        TextStyle {
+                            font: asset_server.load("fonts/goodtimes.otf"),
+                            font_size: 40.0,
+                            color: Color::rgb(0.0, 0.08, 0.20),
+                        },
+                    ));
+                });
+                parent
+                .spawn(ButtonBundle {
+                    style: Style {
+                        size: Size::new(Val::Px(200.0), Val::Px(75.0)),
+                        // horizontally center child text
+                        justify_content: JustifyContent::Center,
+                        // vertically center child text
+                        align_items: AlignItems::Center,
+                        margin: UiRect {
+                            left: Val::Percent(5.),
+                            right: Val::Percent(5.),
+                            top: Val::Percent(5.),
+                            bottom: Val::Percent(5.)
+                        },
+                        ..default()
+                    },
+                    background_color: NORMAL_BUTTON.into(),
+                    ..default()
+                })
+                .with_children(|parent| {
+                    parent.spawn(TextBundle::from_section(
+                        "Exit",
                         TextStyle {
                             font: asset_server.load("fonts/goodtimes.otf"),
                             font_size: 40.0,
@@ -74,15 +103,15 @@ fn button_system(
         let mut text = text_query.get_mut(children[0]).unwrap();
         match *interaction {
             Interaction::Clicked => {
-                text.sections[0].value = "Play".to_string();
+                //text.sections[0].value = "Bzzzz".to_string();
                 *color = PRESSED_BUTTON.into();
             }
             Interaction::Hovered => {
-                text.sections[0].value = "Bzzzz".to_string();
+                //text.sections[0].value = "Bzzzz".to_string();
                 *color = HOVERED_BUTTON.into();
             }
             Interaction::None => {
-                text.sections[0].value = "Play!!!".to_string();
+                //text.sections[0].value = "Play".to_string();
                 *color = NORMAL_BUTTON.into();
             }
         }

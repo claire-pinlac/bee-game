496dea77bf0e0437d1ef6c98a344f5720990a69f
Shnob <jake.dombnz@gmail.com>
2023-07-09 13:27:35 +1200

    Added collision detection

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 //#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 use bevy::prelude::*;
 use bevy_kira_audio::AudioPlugin;
+use bevy_prototype_debug_lines::*;
 
 mod bee_game;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+        .add_plugin(DebugLinesPlugin::default())
         .add_plugin(AudioPlugin)
         .add_plugin(bee_game::BeeGame)
         .run();
